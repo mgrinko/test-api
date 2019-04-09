28b13f9291d2af431bd3c42b366aa2f489dc99f3
mgrinko <glukym@gmail.com>
2019-04-09 09:25:55 +0300

    fix 1

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on http://127.0.0.1:${port}!`))
