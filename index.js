de75acf4b1d9a3acffee9a56eb30d349e751b36d
mgrinko <glukym@gmail.com>
2019-04-09 10:24:46 +0300

    qweqwe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`Hello World! Port: ${port} from Github`));
+app.get('/', (req, res) => res.send(`Hello from Github`));
 
 app.listen(port, () => console.log(
   `Example app listening on http://127.0.0.1:${port}!`,
