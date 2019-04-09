805a711434cad693bc71ae26d70a53ed0f2fc70b
mgrinko <glukym@gmail.com>
2019-04-09 11:01:16 +0300

    POST + body-parser

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 let users = [
   { firstname: 'Misha', lastname: 'Hrynko '},
-  { firstname: 'Petya', lastname: 'Hrynko '},
+  { firstname: 'Petya', lastname: 'Poroh '},
 ];
+
+app.use(bodyParser.text());
 
 app.get('/', (req, res) => res.send(`Hello from Github`));
 app.get('/users', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(users)
+});
+
+app.post('/users', (req, res) => {
+  console.log(req.body);
+
+  res.set('Access-Control-Allow-Origin', '*');
   res.json(users)
 });
 
