db6a877975654a03b2773ebe6d08a900505f6f56
mgrinko <glukym@gmail.com>
2019-04-09 09:31:06 +0300

    fix 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on http://127.0.0.1:${port}!`))
+app.listen(port, () => console.log(
+  `Example app listening on http://127.0.0.1:${port}!`,
+));
