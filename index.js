93f5cb5b71ca9574636600b9c00e025dc2873e58
mgrinko <glukym@gmail.com>
2019-04-09 10:50:30 +0300

    users were move to the server

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+let users = [
+  { firstname: 'Misha', lastname: 'Hrynko '},
+  { firstname: 'Petya', lastname: 'Hrynko '},
+];
+
 app.get('/', (req, res) => res.send(`Hello from Github`));
+app.get('/users', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.json(users)
+});
 
 app.listen(port, () => console.log(
   `Example app listening on http://127.0.0.1:${port}!`,
